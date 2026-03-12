944e6399405a614ef74a61f2ec16ecd58517b15f
aminedevaj <developerwebaj@gmail.com>
2026-03-12 17:45:13 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laundry App</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+
+    <h1 class="text-4xl font-bold text-green-600">
+        Laundry Booking Platform 🚀
+    </h1>
+
+</body>
+</html>
